263936f92e15138da4988fb0965d2d935ec4cc6c
Devas <peperodriguezfdz@hotmail.com>
2019-04-07 17:48:37 +0200

    -Añadimos imagen a los dos tipos de disparos

--- src/graficos/Disparo.java
+++ src/graficos/Disparo.java
@@ -17,5 +17,19 @@ public class Disparo extends Entidad {
     public enum Tipo {//Disparos propios(del jugador) y disparos de las naves enemigas
         NAVE, ENEMIGO
     }
-    
+    private Tipo tipo;//Será el disparo
+
+    public Disparo(Tipo t) {//Constructor del disparo a efectuar según el tipo de disparo que queramos
+        super();
+        if (t == Tipo.ENEMIGO) {
+            setImagen("disparo_enemigo.png");//Creamos el disparo de las naves enemigas
+        }
+        if (t == Tipo.NAVE) {
+            setImagen("disparo.png");//Creamos el disparo de nuestras naves
+        }
+        tipo = t;
+        x = 0;
+        y = 0;
+    }
+
 }
