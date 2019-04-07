dbf13a7285aaedf2c75680d36604ca1e38270c45
Devas <peperodriguezfdz@hotmail.com>
2019-04-07 18:05:05 +0200

    -Creamos el constructor de nuestro juego. Implementamos un timer y una estrategia de doble buffer!

--- src/graficos/Juego.java
+++ src/graficos/Juego.java
@@ -5,8 +5,14 @@
  */
 package graficos;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 /**
  *
@@ -14,6 +20,42 @@ import javax.swing.JPanel;
  */
 public class Juego extends JPanel implements ActionListener {//Aquí vendrá la chicha de nuestro juego
 //La clase Juego extienda la pantalla Jpanel e implementa el reconocimiento de acciones
-    private Nave nave;
+    private Nave nave;//Creamos nuestra nave    
+    private Bandada bandada;//Creamos un grupo de enemigos
+    private Disparo disparoNave;//Nuestros disparos
+    private ArrayList<Disparo> disparosEnemigo;//Lista de disparos enemigos
+
+    public Juego() {
+        nave = new Nave();
+        bandada = new Bandada();
+        disparoNave = new Disparo(Disparo.Tipo.NAVE);
+        disparosEnemigo = new ArrayList<Disparo>();
+
+        addKeyListener(new KeyAdapter() {//Añadimos un keylistener (con este método nuestor programa podrá reconocer las teclas pulsadas (O ...P))
+            @Override
+            public void keyPressed(KeyEvent evento) {
+                nave.teclaPulsada(evento);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent evento) {
+                nave.teclaLiberada(evento);
+            }
+        });
+
+        setFocusable(true);
+        setBackground(Color.WHITE);
+        setDoubleBuffered(true);//Iniciamos el doble buffer para que la actualización por pantalla sea fluida
+
+        Timer timer = new Timer(15, this);//Comenzamos un contador de 15
+        timer.start();//Lo iniciamos
+
+        iniciarJuego();//Iniciamos el juego
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
