ac52444b392294182788aaeaac1d1606f8b0721a
DevasDiCain <peperodriguezfdz@gmail.com>
2019-04-08 16:44:01 +0200

    -Desarrollo del método que comprobará las colisiones propias y enemigas

--- src/graficos/Juego.java
+++ src/graficos/Juego.java
@@ -6,11 +6,16 @@
 package graficos;
 
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
+import java.util.Iterator;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -53,9 +58,66 @@ public class Juego extends JPanel implements ActionListener {//Aquí vendrá la 
         iniciarJuego();//Iniciamos el juego
     }
 
+      public void iniciarJuego() {
+        nave.iniciar();
+        bandada.iniciar();
+    }
+
+    public void paint(Graphics g) {//Creamos el método que nos dibujará en pantalla los elementos contenidos  en los atributos de nuestro juego (nuestra nave, nuestro disparo, enemigos, disparo de enemigos...)
+        super.paint(g);//Hereda de Jpanel
+
+        Graphics2D g2d = (Graphics2D)g;//Instanciamos la clase Graphics2d del paquete atw de java
+        g2d.drawImage(nave.getImagen(), nave.getX(), nave.getY(), this);//Dibujamos nuestra nave
+
+        if (disparoNave.estaActivado()) {
+            g2d.drawImage(disparoNave.getImagen(), disparoNave.getX(), disparoNave.getY(), this);//Si nuestra nave está disparando dibuja sus disparos
+        }
+
+        for (Disparo d: disparosEnemigo) {//Si el enemigos nos dispara , dibujará sus disparos
+            d.actualizar();
+            g2d.drawImage(d.getImagen(), d.getX(), d.getY(), this);
+        }
+
+        for (Enemigo e: bandada.getEnemigos()) {//Dibuja la bandada enemiga
+            if (e.isActivo()) {
+                g2d.drawImage(e.getImagen(), e.getX(), e.getY(), this);
+            }
+        }
+
+        Toolkit.getDefaultToolkit().sync();////Sincronizamos el estado de los graficos/ Al ser una clase abstracta no podemos instanciarla
+        g.dispose();//Liberamos los recursos del sistema utilizados por el elemento Graphics
+    }
+    
     @Override
     public void actionPerformed(ActionEvent ae) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    //AQUI CONTROLAREMOS LOS DISPAROS Y LAS COLISIONES GRACIAS A LOS MÉTODOS IMPLEMENTADOS EN LAS CLASES RESPECTIVAS
+    private void gestionarColisiones() {//Este método controlará las colisiones, es decir, cuando nuestras balas golpean o somos golpeados por los enemigos
+        if (disparoNave.getY() < 0) {//Si el disparo sale de Y entonces lo desactivamos
+            disparoNave.desactivar();
+        }
+        //Daremos "cuerpo" a nuestra bala que avanzará por Y manteniendo su posición en X
+        if (disparoNave.estaActivado()) {//Si estámos disparando
+            Rectangle r = new Rectangle(disparoNave.getX(), disparoNave.getY(), disparoNave.getAlto(), disparoNave.getAncho());//La clase Rectangle especifica un área en unas cordenadas de espacio (x,y). Podemos definirlo dandole un alto y un ancho y el espacio que ocupa
+            if (bandada.comprobarColision(r)) {//Y la bandada enemiga detecta colisión
+                disparoNave.desactivar();//la bala desaparecerá
+            }
+        }
+
+        Iterator<Disparo> i = disparosEnemigo.iterator();//Esto no es más que recorrer varias veces la lista de disparosEnemigos
+        while(i.hasNext()) {//Mientras que tengamos un disparo en la lista disparosEnemigos....(hasnext devuelve true si encuentra un elemento en la lista (DISPAROS))
+            Disparo d = i.next();//Devuelve el siguiente elemento de la lista (disparosEnemigos), que sería el siguiente disparo
+            if (d.getY() > Pantalla.ALTO) {//Si el disparo se sale de Y
+                i.remove();//Lo eliminamos
+            }
+        }
+
+        for (Disparo d: disparosEnemigo) {
+            Rectangle r = new Rectangle(d.getX(), d.getY(), d.getAlto(), d.getAncho());//Le asignamos una zona a los disparos enemigos
+            nave.comprobarColision(r);//Comprobamos si le dan a nuestra nave
+        }
     }
 
 }
